f028e0a018856d3ac17a0e7b20121937c829ddfe
rathinchopra98 <choprrat@sheridancollege.ca>
2020-02-16 10:47:39 -0500

    Added Login Page UI

---
+++ BestBuyVision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestBuyVision
+//
+//  Created by Rathin Chopra on 2020-02-16.
+//  Copyright © 2020 Rathin Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
