37d3fb0d9e2f91aa5808ddbb70b8b7ff48115891
rathinchopra98 <choprrat@sheridancollege.ca>
2020-02-16 22:08:32 -0500

    commit

--- BestBuyVision/ViewController.swift
+++ BestBuyVision/ViewController.swift
@@ -36,6 +36,10 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "segueSignUp", sender: nil)
     }
     
+    @IBAction func unwindToThisView(sender: UIStoryboardSegue) {
+        
+    }
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
