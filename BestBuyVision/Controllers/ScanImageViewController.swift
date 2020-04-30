c36b8a77b92d3b85afcc055f2cea6081bfe134f0
rathinchopra98 <choprrat@sheridancollege.ca>
2020-04-30 19:12:34 -0400

    Added custom model for image recognition

--- BestBuyVision/Controllers/ScanImageViewController.swift
+++ BestBuyVision/Controllers/ScanImageViewController.swift
@@ -93,7 +93,7 @@ class ScanImageViewController: UIViewController, UINavigationControllerDelegate,
              To use a different Core ML classifier model, add it to the project
              and replace `MobileNet` with that model's generated Swift class.
              */
-            let model = try VNCoreMLModel(for: Capstone_Image_Classifier().model)
+            let model = try VNCoreMLModel(for: CIFAR().model)
             
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
